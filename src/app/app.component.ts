a520ab5c46791ba269e7d7c978cc8c5c99cdcd60
thernande <tehp1993@gmail.com>
2017-09-08 23:16:43 -0500

    angular instalacion de bootstrap y jquery en el archivo angular-cli.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, DoCheck, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements DoCheck, OnInit {
-  title = 'Curso de Angular 4 Avanzado';
+  title = 'NgZOO';
   email: string;
 
   ngOnInit(){
