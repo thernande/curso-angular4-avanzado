d8b72dd3262159578b02f988294b38e351369ac3
thernande <tehp1993@gmail.com>
2017-10-21 20:25:51 -0500

    se pone en funcionamiento el listado de animales

--- src/app/admin/components/list/list.component.ts
+++ src/app/admin/components/list/list.component.ts
@@ -1,16 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { GLOBAL } from '../../../services/global';
+import { AnimalService } from '../../../services/animal.service';
+import { UserService } from '../../../services/user.service';
+import { UploadService } from '../../../services/upload.service';
+import { Animal } from '../../../models/animal';
 
 @Component({
   selector: 'admin-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  providers: [AnimalService]
 })
 export class ListComponent implements OnInit {
-	title = "lista";
-	numbers = new Array(10);
-  constructor() { }
+  public title: string;
+  public animals: Animal[];
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _animalService: AnimalService
+  ) {
+    this.title = 'Listado de animales';
+  }
 
   ngOnInit() {
+    this._animalService.getAnimals().subscribe(
+      response => {
+        if(response.animals){
+          this.animals = response.animals;
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
   }
 
 }
