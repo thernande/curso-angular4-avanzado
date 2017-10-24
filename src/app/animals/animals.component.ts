072b324e5a431c02c7faa2e1bd6b6248cc51e421
thernande <tehp1993@gmail.com>
2017-10-24 17:14:27 -0500

    se realiza la pagina de animales en la opciones principales

--- src/app/animals/animals.component.ts
+++ src/app/animals/animals.component.ts
@@ -1,17 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from '../models/animal';
+import { AnimalService } from '../services/animal.service';
+import { GLOBAL } from '../services/global';
 
 @Component({
   selector: 'app-animals',
   templateUrl: './animals.component.html',
-  styleUrls: ['./animals.component.css']
+  styleUrls: ['./animals.component.css'],
+  providers: [AnimalService]
 })
 export class AnimalsComponent implements OnInit {
-	public title = 'Animal';
+	public title: string;
+  public animals: Animal;
+  public url: string;
 
-  constructor() { }
+  constructor(
+    private _animalService: AnimalService
+  ) {
+    this.title = "Animales"
+    this.url = GLOBAL.url;
+  }
 
   ngOnInit() {
-  	console.log('keeper.component cargado!!!');
+  	this.getAnimals();
+
+  }
+
+  getAnimals(){
+    this._animalService.getAnimals().subscribe(
+      response => {
+        if(response.animals){
+          this.animals = response.animals;
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
   }
 
 }
